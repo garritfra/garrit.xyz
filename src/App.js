1a40287522c06fb16ceb444d81ef049b43f52ebe
garritfra <32395585+garritfra@users.noreply.github.com>
2018-01-05 13:49:19 +0100

    add test component button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "./components/Button"
+import Button from './components/Button';
 
 class App extends Component {
   render() {
@@ -10,6 +12,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <Button/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+    render() {
+        return (
+            <div>
+                <button>Test</button>
+            </div>
+    );
+    }
+}
+
+
+export default Button;
