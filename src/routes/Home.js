f6d20c7a5332918f78fad5067bffb487f4b65f13
garritfra <32395585+garritfra@users.noreply.github.com>
2018-01-15 17:11:00 +0100

    add image to info section

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import '../index.css';
 import ProjectPreview from './../components/ProjectPreview';
+import Info from '../components/Info';
 
 class Home extends Component {
 
@@ -9,6 +10,7 @@ class Home extends Component {
       return (
   
         <div>
+          <Info/>
           <ProjectPreview />
         </div>
       );
