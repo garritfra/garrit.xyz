a8c1db5f53ea75a2b73315ffd5ee698df6c4fd1f
garritfra <32395585+garritfra@users.noreply.github.com>
2018-01-17 00:58:22 +0100

    fix linting in sketch.js

--- src/components/projects/PerlinNoise/sketch.js
+++ src/components/projects/PerlinNoise/sketch.js
@@ -1,45 +1,42 @@
-export default function sketch(p) {
+import p5 from 'p5';
 
-    var w = window.innerWidth * 0.80;
-    var h = window.innerHeight * 0.80;
-    var xoff = 0;       //X Offset
-    var yoff = 100000;  //Y Offset
-    var coff1 = 100;    //Color 1 Offset
-    var coff2 = 1000;   //Color 2 Offset
-    var coff3 = 10000;  //Color 3 Offset
-    var soff = 30;      //Scale Offset
+export default function sketch() {
+  const w = p5.window.innerWidth * 0.80;
+  const h = p5.window.innerHeight * 0.80;
+  let xoff = 0; // X Offset
+  let yoff = 100000; // Y Offset
+  let coff1 = 100; // Color 1 Offset
+  let coff2 = 1000; // Color 2 Offset
+  let coff3 = 10000; // Color 3 Offset
+  let soff = 30; // Scale Offset
 
-    p.setup = () => {
-        p.createCanvas(w, h);
-        p.background(50);
-        //noStroke();
-    }
+  p5.setup = () => {
+    p5.createCanvas(w, h);
+    p5.background(50);
+    // noStroke();
+  };
 
-    p.draw = () => {
+  p5.draw = () => {
+    // background(255, 255,255, 0.5);
 
-        //background(255, 255,255, 0.5);
+    const x = p5.noise(xoff) * w;
+    const y = p5.noise(yoff) * h;
+    const c1 = p5.noise(coff1) * 255;
+    const c2 = p5.noise(coff2) * 255;
+    const c3 = p5.noise(coff3) * 255;
+    const c = p5.color(c1, c2, c3);
+    const s = p5.noise(soff) * 50;
 
-        var x = p.noise(xoff) * w;
-        var y = p.noise(yoff) * h;
-        var c1 = p.noise(coff1) * 255;
-        var c2 = p.noise(coff2) * 255;
-        var c3 = p.noise(coff3) * 255;
-        var c = p.color(c1, c2, c3);
-        var s = p.noise(soff) * 50;
-
-        p.fill(c);
-
-
-        p.ellipse(x, y, s);
-
-        xoff += 0.01;
-        yoff += 0.01;
-        coff1 += 0.02;
-        coff2 += 0.02;
-        coff3 += 0.02;
-        soff += 0.01;
+    p5.fill(c);
 
 
+    p5.ellipse(x, y, s);
 
-    }
+    xoff += 0.01;
+    yoff += 0.01;
+    coff1 += 0.02;
+    coff2 += 0.02;
+    coff3 += 0.02;
+    soff += 0.01;
+  };
 }
