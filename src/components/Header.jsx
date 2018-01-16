47a45309e3591f609c05a9c39a46e83df9136a63
garritfra <32395585+garritfra@users.noreply.github.com>
2018-01-16 17:19:13 +0100

    add facebook contact

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,14 +30,16 @@ class Header extends Component {
 
     return (
       <div>
-        <Menu inverted size='big'>
+        <Menu inverted size='large'>
           <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleHome} />
           <Menu.Item name='projects' active={activeItem === 'projects'} onClick={this.handleProjects} />
 
           <Menu.Menu position='right'>
-
-            <Menu.Item href="https://github.com/garritfra" target= "_blank">
-              <Icon name="github" size="large"/>
+            <Menu.Item href="https://github.com/garritfra" target="_blank">
+              <Icon link name="github" size="large" />
+            </Menu.Item>
+            <Menu.Item href="https://www.facebook.com/Garritfranke" target="_blank">
+              <Icon link name="facebook" size="large" />
             </Menu.Item>
           </Menu.Menu>
         </Menu>
