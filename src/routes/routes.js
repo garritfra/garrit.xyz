5cce8d5fef7ba1fd6fe29ce7dc86f40a929d17e9
garritfra <32395585+garritfra@users.noreply.github.com>
2018-01-17 00:53:11 +0100

    fix linting in routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,16 +1,17 @@
-import _ from "lodash"
-import NotFound from "./NotFound";
+import _ from 'lodash';
+import NotFound from './NotFound';
 
-export let routes = [
-    { id: 0, component: "Home", link: "/home" },
-    { id: 1, component: "Projects", link: "/projects" },
-    { id: 2, component: NotFound, link: "/*" }
+export const routes = [
+  { id: 0, component: 'Home', link: '/home' },
+  { id: 1, component: 'Projects', link: '/projects' },
+  { id: 2, component: NotFound, link: '/*' },
 
-    
-]
 
-export let createRoute = (component, link) => {
-    let id = _.lastIndexOf(routes)
+];
 
-    routes.push({ id: id, component: component, link: link })
-}
+export const createRoute = (component, link) => {
+  const id = _.lastIndexOf(routes);
+
+  routes.push({ id, component, link });
+};
+
