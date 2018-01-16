1f0fc8610df33bff46a88a9e938773c6b996bb6a
garritfra <32395585+garritfra@users.noreply.github.com>
2018-01-16 16:35:32 +0100

    add method cutString to projectCard

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Card, Icon } from 'semantic-ui-react'
 import { withRouter } from "react-router-dom"
+import _ from "lodash";
 
 
 class ProjectCard extends Component {
@@ -10,11 +11,21 @@ class ProjectCard extends Component {
 		this.props = props;
 	}
 	cardStyle = {
-		//marginTop: 30,
+		marginTop: 30,
 		padding: "10%",
 		Height: "1em",
 		width: "100%"
 		
+	}
+
+	cutString = (str, len) => {
+
+		if (_.lastIndexOf(str) > len){
+			return str.slice(0, len) + ".."
+		}
+		else{
+			return str;
+		}
 	}
 
 	render() {
@@ -26,7 +37,7 @@ class ProjectCard extends Component {
 				<Card style={this.cardStyle}>
 					<Card.Content header={this.props.project.name}/>
 					<Card.Content description=
-							{this.props.project.description}/>
+							{this.cutString(this.props.project.description, 30)}/>
 						<Card.Content button href={"/projects" + this.props.project.path} color="primary">Go to Project</Card.Content>
 				</Card>
 			</div>
