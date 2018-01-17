7924c593f00bfa3c5bf78dd2d109613accaecbd3
garritfra <32395585+garritfra@users.noreply.github.com>
2018-01-17 01:02:04 +0100

    revert linting for sketch.js

--- src/components/projects/PerlinNoise/sketch.js
+++ src/components/projects/PerlinNoise/sketch.js
@@ -1,8 +1,6 @@
-import p5 from 'p5';
-
-export default function sketch() {
-  const w = p5.window.innerWidth * 0.80;
-  const h = p5.window.innerHeight * 0.80;
+export default function sketch(p) {
+  const w = window.innerWidth * 0.80;
+  const h = window.innerHeight * 0.80;
   let xoff = 0; // X Offset
   let yoff = 100000; // Y Offset
   let coff1 = 100; // Color 1 Offset
@@ -10,27 +8,27 @@ export default function sketch() {
   let coff3 = 10000; // Color 3 Offset
   let soff = 30; // Scale Offset
 
-  p5.setup = () => {
-    p5.createCanvas(w, h);
-    p5.background(50);
+  p.setup = () => {
+    p.createCanvas(w, h);
+    p.background(50);
     // noStroke();
   };
 
-  p5.draw = () => {
+  p.draw = () => {
     // background(255, 255,255, 0.5);
 
-    const x = p5.noise(xoff) * w;
-    const y = p5.noise(yoff) * h;
-    const c1 = p5.noise(coff1) * 255;
-    const c2 = p5.noise(coff2) * 255;
-    const c3 = p5.noise(coff3) * 255;
-    const c = p5.color(c1, c2, c3);
-    const s = p5.noise(soff) * 50;
+    const x = p.noise(xoff) * w;
+    const y = p.noise(yoff) * h;
+    const c1 = p.noise(coff1) * 255;
+    const c2 = p.noise(coff2) * 255;
+    const c3 = p.noise(coff3) * 255;
+    const c = p.color(c1, c2, c3);
+    const s = p.noise(soff) * 50;
 
-    p5.fill(c);
+    p.fill(c);
 
 
-    p5.ellipse(x, y, s);
+    p.ellipse(x, y, s);
 
     xoff += 0.01;
     yoff += 0.01;
