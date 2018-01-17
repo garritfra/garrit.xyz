e5d69048939217b7ca7c45a4c19ee7cb66cd4ace
garritfra <32395585+garritfra@users.noreply.github.com>
2018-01-17 12:54:14 +0100

    add enzyme dependency

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
 import App from '../App';
 
-
-describe("App", () => {
+describe('App', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
