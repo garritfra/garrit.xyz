98c16b1ceb5d91d7cfa0c7a27dc306626a92033c
garritfra <32395585+garritfra@users.noreply.github.com>
2018-01-17 12:47:58 +0100

    purify app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
-import './App.css';
-import {Router, Route, Switch, } from "react-router-dom"
-import Home from "./routes/Home";
-import Projects from "./routes/Projects"
-import NotFound from "./routes/NotFound"
-import Header from './components/Header';
 import createBrowserHistory from 'history/createBrowserHistory';
-import PerlinNoise from './components/projects/PerlinNoise/PerlinNoise';
+import { Router, Route, Switch } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
+import './App.css';
+import Home from './routes/Home';
+import Projects from './routes/Projects';
+import NotFound from './routes/NotFound';
+import Header from './components/Header';
+import PerlinNoise from './components/projects/PerlinNoise/PerlinNoise';
 
 class App extends Component {
-
+  constructor(props) {
+    super();
+    this.props = props;
+  }
   render() {
     const history = createBrowserHistory();
     return (
@@ -19,10 +22,10 @@ class App extends Component {
         <div className="App">
           <Header history={history} />
           <Switch>
-            <Route path="/" exact render={(props) => <Home />} />
+            <Route path="/" exact render={props => <Home />} />
             <Route path="/projects/perlinnoise" render={() => <PerlinNoise />} />
             <Route path="/projects/*" render={() => <NotFound />} />
-            <Route path="/projects" render={() => <Projects/>}/>
+            <Route path="/projects" render={() => <Projects />} />
             <Route path="/*" render={() => <NotFound />} />
           </Switch>
         </div>
