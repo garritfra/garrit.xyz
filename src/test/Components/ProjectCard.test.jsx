0a1eb0e337225cad13a7c8e306fb37863ddfa941
garritfra <32395585+garritfra@users.noreply.github.com>
2018-01-17 16:04:16 +0100

    add test adapter to ProjectCard

--- src/test/Components/ProjectCard.test.jsx
+++ src/test/Components/ProjectCard.test.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from '../../App';
 import ProjectCard from '../../components/ProjectCard';
 
-// Enzyme.configure({ Adapter: new Adapter() });
+Enzyme.configure({ Adapter: new Adapter() });
 
 const div = document.createElement('div');
 const state = {
@@ -28,6 +30,7 @@ const state = {
 
 describe('ProjectCard', () => {
   it('renders without crashing', () => {
-    ReactDOM.render(<ProjectCard project={state.projects[0]} />, div);
+    const card = shallow(<ProjectCard project={state.projects[0]} />, div);
+
   });
 });
