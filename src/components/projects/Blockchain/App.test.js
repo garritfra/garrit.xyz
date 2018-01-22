7587176052a501ccd88389df7e10f35429b2088f
garritfra <32395585+garritfra@users.noreply.github.com>
2018-01-22 19:33:42 +0100

    fix test with wrong component reference

--- src/components/projects/Blockchain/App.test.js
+++ src/components/projects/Blockchain/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import BlockchainApp from './BlockchainApp';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<BlockchainApp />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
