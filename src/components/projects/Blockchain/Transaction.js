8cf21e542e7b47dd0eeb31c7f2a6b6c74de63338
garritfra <32395585+garritfra@users.noreply.github.com>
2018-01-22 21:18:04 +0100

    JSDoc for transaction class

--- src/components/projects/Blockchain/Transaction.js
+++ src/components/projects/Blockchain/Transaction.js
@@ -1,4 +1,13 @@
+
 export default class Transaction {
+/**
+ * creates a transaction object
+ *
+ * @constructor
+ * @param {string} sender
+ * @param {string} receiver
+ * @param {number} amount
+ */
   constructor(sender, receiver, amount) {
     this.sender = sender;
     this.receiver = receiver;
