2cfc7893477df7a2f58ae0c05113753760969bda
garritfra <garritfranke@gmail.com>
2018-04-06 16:58:48 +0200

    add particles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 
+import Home from "./Home/Home";
+
 export default class App extends Component {
   render() {
-    return(<h1>Hello World</h1>)
+    return (
+      <div>
+        <Home />
+      </div>
+    );
   }
 }
