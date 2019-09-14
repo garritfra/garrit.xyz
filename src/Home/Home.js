79ef97ea46e975cd9c64726b2f7702a052d07a5b
garritfra <garritfranke@gmail.com>
2019-09-14 22:43:44 +0200

    Add Typewriter effect

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 import Particles from "react-particles-js";
 import "./Home.scss";
 import github from "./GitHub.svg";
-import linkedin from "./LinkedIn.svg";
-import LinkItem from "./LinkItem";
+import Typed from "react-typed";
 
 export default class Home extends Component {
   constructor(props) {
@@ -20,8 +19,24 @@ export default class Home extends Component {
           <div className="section-header">
             <h1 className="animated fadeInLeft headline">Hi, I'm Garrit</h1>
             <p id="subheadline" className="animated fadeInRight">
-              Software Development is my Passion. Don't hesitate to check out my
-              projects!
+              I love{" "}
+              <Typed
+                strings={[
+                  "JavaScript",
+                  "doing web stuff with React",
+                  "doing web stuff with Express.js",
+                  "building Apps for Android",
+                  "building Apps for iOS",
+                  "building Apps with Flutter",
+                  "going deep with Rust",
+                  "you! ❤️"
+                ]}
+                typeSpeed={50}
+                backSpeed={40}
+                backDelay={1000}
+                startDelay={1000}
+                smartBackspace
+              />
             </p>
           </div>
         </div>
