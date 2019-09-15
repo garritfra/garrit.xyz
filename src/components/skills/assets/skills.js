f90cb2f61f25714171b88a6c4167da75ff7af95f
garritfra <garritfranke@gmail.com>
2019-09-15 20:19:22 +0200

    Add Frontend Skills

--- src/components/skills/assets/skills.js
+++ src/components/skills/assets/skills.js
@@ -6,6 +6,36 @@ export default [
         name: "HTML",
         logo: require("./html.svg"),
         rating: 5
+      },
+      {
+        name: "CSS",
+        logo: require("./css.svg"),
+        rating: 3
+      },
+      {
+        name: "Node.js",
+        logo: require("./nodejs.svg"),
+        rating: 5
+      },
+      {
+        name: "React",
+        logo: require("./react.svg"),
+        rating: 5
+      },
+      {
+        name: "Bootstrap",
+        logo: require("./bootstrap.svg"),
+        rating: 3
+      },
+      {
+        name: "Jest",
+        logo: require("./jest.svg"),
+        rating: 3
+      },
+      {
+        name: "ReactiveX",
+        logo: require("./reactivex.svg"),
+        rating: 4
       }
     ]
   },
@@ -15,16 +45,11 @@ export default [
   },
 
   {
-    name: "Mobile",
-    skills: [
-      {
-        name: "Android",
-        skills: [{ name: "Java", logo: require("./java.svg"), rating: 5 }]
-      },
-      {
-        name: "iOS",
-        skills: [{ name: "Swift", logo: require("./swift.svg"), rating: 4 }]
-      }
-    ]
+    name: "Android",
+    skills: [{ name: "Android", logo: require("./android.svg"), rating: 5 }]
+  },
+  {
+    name: "iOS",
+    skills: [{ name: "iOS", logo: require("./ios.svg"), rating: 4 }]
   }
 ];
