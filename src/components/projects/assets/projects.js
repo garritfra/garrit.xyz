9f8c88a4a818f6c25ccd3bd66ac941cfc7652aa8
garritfra <garritfranke@gmail.com>
2020-06-09 16:18:12 +0200

    feat: add multiple projects

--- src/components/projects/assets/projects.js
+++ src/components/projects/assets/projects.js
@@ -9,5 +9,22 @@ export default [
         year: "2020"
       }
     ]
+  },
+  {
+    name: "Systems",
+    projects: [
+      {
+        name: "Ditto",
+        description: "A simple educational blockchain concept",
+        url: "https://github.com/garritfra/blockchain-project",
+        year: "2018"
+      },
+      {
+        name: "Rustfuck",
+        description: "A brainfuck compiler written in rust",
+        url: "https://github.com/garritfra/rustfuck",
+        year: "2018"
+      }
+    ]
   }
 ];
