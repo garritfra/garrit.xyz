86bd0f05b90fd323fea2a5beab537e258a0657fd
Garrit Franke <garrit@slashdev.space>
2021-08-29 20:53:57 +0200

    feat: animated header text

--- components/Header.js
+++ components/Header.js
@@ -1,30 +1,19 @@
-import { useLayoutEffect, useState } from "react";
-
-function useWindowSize() {
-    const [size, setSize] = useState([0, 0]);
-    useLayoutEffect(() => {
-        function updateSize() {
-            setSize([window.innerWidth, window.innerHeight]);
-        }
-        window.addEventListener("resize", updateSize);
-        updateSize();
-        return () => window.removeEventListener("resize", updateSize);
-    }, []);
-    return size;
-}
+import TypeIt from "typeit-react";
 
 export default function Header(props) {
-    const [windowWidth, windowHeight] = useWindowSize();
-
     return (
         <header className="header">
             <nav className="nav" role="navigation" aria-label="main navigation">
-                <a href="/">
-                    <h1>{props.siteTitle}</h1>
+                <a href="/" className="header__logo">
+                    <TypeIt options={{ strings: [props.siteTitle], speed: 30, lifeLike: true }}>{props.siteTitle}</TypeIt>
                 </a>
                 <ul className="header__links">
-                    <li><a href="/posts">Blog</a></li>
-                    <li><a href="/cv">Resume</a></li>
+                    <li>
+                        <a href="/posts">Blog</a>
+                    </li>
+                    <li>
+                        <a href="/cv">Resume</a>
+                    </li>
                 </ul>
             </nav>
         </header>
