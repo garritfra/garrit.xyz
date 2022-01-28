d2e33505fa62b8a364540cac9d509d453c8db0cf
Garrit Franke <garrit@slashdev.space>
2022-01-28 17:25:30 +0100

    blog: add anchor tags for posts

--- pages/posts/[post].js
+++ pages/posts/[post].js
@@ -4,6 +4,7 @@ import gfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
 import Page from "../../components/Page";
 import glob from "glob";
+import React from "react";
 
 export default function BlogTemplate(props) {
     function reformatDate(fullDate) {
@@ -19,6 +20,15 @@ export default function BlogTemplate(props) {
 
     if (!props.frontmatter) return <></>;
 
+    const addAnchorTag = ({ node, children, ...props }) => {
+        const headerSlug = node.children[0].value.replaceAll(" ", "-").toLowerCase();
+        return React.createElement(node.tagName, { 
+            id: headerSlug,
+            children: [children],
+            ...props,
+        });
+    }
+
     return (
         <Page
             siteTitle="Garrit's Notes"
@@ -29,6 +39,14 @@ export default function BlogTemplate(props) {
                 date={props.frontmatter.date}
                 remarkPlugins={[gfm]}
                 rehypePlugins={[rehypeRaw]}
+                components={{
+                    h1: addAnchorTag,
+                    h2: addAnchorTag,
+                    h3: addAnchorTag,
+                    h4: addAnchorTag,
+                    h5: addAnchorTag,
+                    h6: addAnchorTag,
+                  }}
             >
                 {props.markdownBody}
             </ReactMarkdown>
