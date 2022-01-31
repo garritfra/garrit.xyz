884cf22a5891bf61bb05befa33cb823db46b0e16
Garrit Franke <garrit@slashdev.space>
2022-01-31 10:28:35 +0100

    indieweb: add h-entry classes

--- components/Page.js
+++ components/Page.js
@@ -4,12 +4,12 @@ export default function Page(props) {
     const { title, date, siteTitle } = props;
     return (
         <Layout siteTitle={siteTitle}>
-            <article className="page">
+            <article className="page e-entry">
                 { title && <div className="page__info">
-                    <h1>{title}</h1>
+                    <h1 className="p-name">{title}</h1>
                     {date && <h3 className="page__info__date">{date}</h3>}
                 </div> }
-                <div className="page__body">{props.children}</div>
+                <div className="page__body e-content">{props.children}</div>
                 <div className="page__footer"></div>
             </article>
         </Layout>
