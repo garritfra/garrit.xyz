66b39a98ded56abd7ced479af88b40f3eb282299
Garrit Franke <garrit@slashdev.space>
2022-03-26 21:28:02 +0100

    blog: simplify blog list

--- components/BlogList.js
+++ components/BlogList.js
@@ -1,42 +1,25 @@
-import ReactMarkdown from "react-markdown";
-import gfm from "remark-gfm";
-
 const BlogList = ({ posts }) => {
     const reformatDate = (fullDate) => {
         const date = new Date(fullDate);
         return date.toDateString().slice(4);
     };
-    const truncateSummary = (content) => {
-        const firstParagraph = content.split("\n\n")[0];
-        if (firstParagraph.length > 200) {
-            return content.slice(0, 200).trimEnd() + "...";
-        } else {
-            return firstParagraph;
-        }
-    };
+
+    const isPublicPost = (post) => !post.slug.startsWith("_");
 
     const renderPost = (post) => (
-        <div className="blog__list__post" key={post.slug}>
-            <a href={`/posts/${post.slug}`}>
-                <h2>{post.frontmatter.title}</h2>
-                <h4 className="blog__list__post__date">
-                    {reformatDate(post.frontmatter.date)}
-                </h4>
-                <p>
-                    <ReactMarkdown remarkPlugins={[gfm]}>
-                        {truncateSummary(post.markdownBody)}
-                    </ReactMarkdown>
-                </p>
-            </a>
+        <div key={post.slug} className="blog__list__post">
+            <p className="blog__list__post__date">
+                {reformatDate(post.frontmatter.date)}
+            </p>
+            <a href={`/posts/${post.slug}`}>{post.frontmatter.title}</a>
         </div>
     );
 
     return (
-        <div className="blog__list">
+        <div>
             {posts.length > 0 &&
                 posts
-                    // Filter drafts
-                    .filter((post) => !post.slug.startsWith("_"))
+                    .filter(isPublicPost)
                     // Ternary operator is used to fix chromium sorting
                     // See: https://stackoverflow.com/a/36507611
                     .sort((a, b) =>
