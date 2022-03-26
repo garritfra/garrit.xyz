47cc7df75612096201562ca5cbcc632f7d6712ab
Garrit Franke <garrit@slashdev.space>
2022-03-26 21:06:31 +0100

    blog: refactor bloglist component

--- components/BlogList.js
+++ components/BlogList.js
@@ -1,21 +1,36 @@
 import ReactMarkdown from "react-markdown";
-import gfm from 'remark-gfm'
-
-function reformatDate(fullDate) {
-    const date = new Date(fullDate);
-    return date.toDateString().slice(4);
-}
-
-function truncateSummary(content) {
-    const firstParagraph = content.split("\n\n")[0];
-    if (firstParagraph.length > 200) {
-        return content.slice(0, 200).trimEnd() + "...";
-    } else {
-        return firstParagraph;
-    }
-}
+import gfm from "remark-gfm";
 
 const BlogList = ({ posts }) => {
+    const reformatDate = (fullDate) => {
+        const date = new Date(fullDate);
+        return date.toDateString().slice(4);
+    };
+    const truncateSummary = (content) => {
+        const firstParagraph = content.split("\n\n")[0];
+        if (firstParagraph.length > 200) {
+            return content.slice(0, 200).trimEnd() + "...";
+        } else {
+            return firstParagraph;
+        }
+    };
+
+    const renderPost = (post) => (
+        <div className="blog__list__post" key={post.slug}>
+            <a href={`/posts/${post.slug}`}>
+                <h2>{post.frontmatter.title}</h2>
+                <h4 className="blog__list__post__date">
+                    {reformatDate(post.frontmatter.date)}
+                </h4>
+                <p>
+                    <ReactMarkdown remarkPlugins={[gfm]}>
+                        {truncateSummary(post.markdownBody)}
+                    </ReactMarkdown>
+                </p>
+            </a>
+        </div>
+    );
+
     return (
         <div className="blog__list">
             {posts.length > 0 &&
@@ -27,21 +42,7 @@ const BlogList = ({ posts }) => {
                     .sort((a, b) =>
                         a.frontmatter.date < b.frontmatter.date ? 1 : -1
                     )
-                    .map((post) => (
-                        <div className="blog__list__post" key={post.slug}>
-                            <a href={`/posts/${post.slug}`}>
-                                <h2>{post.frontmatter.title}</h2>
-                                <h4 className="blog__list__post__date">
-                                    {reformatDate(post.frontmatter.date)}
-                                </h4>
-                                <p>
-                                    <ReactMarkdown remarkPlugins={[gfm]}>
-                                        {truncateSummary(post.markdownBody)}
-                                    </ReactMarkdown>
-                                </p>
-                            </a>
-                        </div>
-                    ))}
+                    .map(renderPost)}
         </div>
     );
 };
