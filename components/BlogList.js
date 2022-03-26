82879e526a2a06498f742fb1c606c3e9ec41e904
Garrit Franke <garrit@slashdev.space>
2022-03-26 21:43:32 +0100

    blog: add random button

--- components/BlogList.js
+++ components/BlogList.js
@@ -1,10 +1,21 @@
 const BlogList = ({ posts }) => {
+    const isPublicPost = (post) => !post.slug.startsWith("_");
+    const publicPosts = posts.filter(isPublicPost);
+
     const reformatDate = (fullDate) => {
         const date = new Date(fullDate);
         return date.toDateString().slice(4);
     };
 
-    const isPublicPost = (post) => !post.slug.startsWith("_");
+    const renderRandomButton = () => {
+        const randomIndex = Math.floor(Math.random() * publicPosts.length);
+        const randomPost = publicPosts[randomIndex];
+        return (
+            <p>
+                <a href={`/posts/${randomPost?.slug}`}>✨ Random Post ✨</a>
+            </p>
+        );
+    };
 
     const renderPost = (post) => (
         <div key={post.slug} className="blog__list__post">
@@ -16,17 +27,21 @@ const BlogList = ({ posts }) => {
     );
 
     return (
-        <div>
-            {posts.length > 0 &&
-                posts
-                    .filter(isPublicPost)
-                    // Ternary operator is used to fix chromium sorting
-                    // See: https://stackoverflow.com/a/36507611
-                    .sort((a, b) =>
-                        a.frontmatter.date < b.frontmatter.date ? 1 : -1
-                    )
-                    .map(renderPost)}
-        </div>
+        <>
+            {renderRandomButton()}
+            <hr />
+            <div>
+                {publicPosts.length > 0 &&
+                    publicPosts
+                        .filter(isPublicPost)
+                        // Ternary operator is used to fix chromium sorting
+                        // See: https://stackoverflow.com/a/36507611
+                        .sort((a, b) =>
+                            a.frontmatter.date < b.frontmatter.date ? 1 : -1
+                        )
+                        .map(renderPost)}
+            </div>
+        </>
     );
 };
 
