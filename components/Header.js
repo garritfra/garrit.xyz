584723692cc31b7b055883da66be759ff802cad6
Garrit Franke <garrit@appmotion.de>
2022-05-24 09:44:08 +0200

    Revert "ukraine: add donate banner"
    This reverts commit 3272f07439f782c4503a9e3eaf453e5d9542c4be.

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,6 @@
 export default function Header(props) {
     return (
         <header className="header">
-            <a
-                href="https://www.comebackalive.in.ua/"
-                style={{ height: "64px", textAlign: "center" }}
-                onClick={() => plausible("ukraine_banner_clicked")}
-                className="flag--ukraine"
-                target="_blank"
-            >
-                Help Ukraine
-            </a>
             <nav className="nav" role="navigation" aria-label="main navigation">
                 <div className="header__container">
                     <a href="/" className="header__container__logo underlined">
