8babd3bf8eca30f3b1a5ef61e0d1155e68e6841a
Garrit Franke <garrit@slashdev.space>
2022-07-14 23:05:56 +0200

    ctf: add jurassic park app

---
+++ lib/jurassic.garrit.xyz/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Methods", "HACK")
+	if r.Method != "HACK" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	magicWord := r.Header.Get("Magic-Word")
+	if strings.ToLower(magicWord) != "please" {
+		fmt.Fprintf(w, "Na ah ah, you didn't say the Magic-Word!\n")
+		fmt.Fprintf(w, "Na ah ah, you didn't say the Magic-Word!\n")
+		fmt.Fprintf(w, "Na ah ah, you didn't say the Magic-Word!\n")
+		fmt.Fprintf(w, "Na ah ah, you didn't say the Magic-Word!\n")
+		fmt.Fprintf(w, "Na ah ah, you didn't say the Magic-Word!\n")
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+
+	fmt.Fprintf(w, "GXYZ{w31c0m3_2_ju24551c_p42k}")
+
+}
+
+func main() {
+	handler := http.HandlerFunc(handleRequest)
+	http.Handle("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
