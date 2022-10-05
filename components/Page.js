658efa3a3ebfebebbf74d0eb6aae6c1cc9566516
Garrit Franke <garrit@slashdev.space>
2022-10-05 11:58:25 +0200

    Add edit hotkey

--- components/Page.js
+++ components/Page.js
@@ -1,7 +1,26 @@
 import Layout from "./Layout";
 
+const SSR = typeof window === 'undefined'
+
 export default function Page(props) {
     const { title, date, siteTitle } = props;
+
+    const setupEditHotkey = () => {
+        window.addEventListener("keypress", (e) => {
+            if (e.key === ".") {
+                const baseUrl =
+                    "https://github.com/garritfra/garrit.xyz/edit/main/content";
+                const filePath = window.location.pathname;
+                const url = `${baseUrl}${filePath}.md`;
+    
+                window.location.href = url;
+            }
+        });
+
+    }
+
+    !SSR && setupEditHotkey();
+
     return (
         <Layout siteTitle={siteTitle} pageTitle={title}>
             <article className="page h-entry">
