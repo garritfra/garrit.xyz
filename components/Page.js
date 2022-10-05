8c374a8bc0b66192c454300489fee52e7299c9dd
Garrit Franke <garrit@slashdev.space>
2022-10-05 12:13:42 +0200

    Add /edit routes

--- components/Page.js
+++ components/Page.js
@@ -1,35 +1,35 @@
 import Layout from "./Layout";
 
-const SSR = typeof window === 'undefined'
+const SSR = typeof window === "undefined";
 
 export default function Page(props) {
-    const { title, date, siteTitle } = props;
+	const { title, date, siteTitle } = props;
 
-    const setupEditHotkey = () => {
+	const setupEditHook = () => {
         window.addEventListener("keypress", (e) => {
-            if (e.key === ".") {
-                const baseUrl =
-                    "https://github.com/garritfra/garrit.xyz/edit/main/content";
-                const filePath = window.location.pathname;
-                const url = `${baseUrl}${filePath}.md`;
-    
-                window.location.href = url;
-            }
-        });
+			const baseUrl =
+				"https://github.com/garritfra/garrit.xyz/edit/main/content";
+			if (e.key === ".") {
+				const filePath = window.location.pathname;
+				const url = `${baseUrl}${filePath}.md`;
+				window.location.href = url;
+			}
+		});
+	};
 
-    }
+	!SSR && setupEditHook();
 
-    !SSR && setupEditHotkey();
-
-    return (
-        <Layout siteTitle={siteTitle} pageTitle={title}>
-            <article className="page h-entry">
-                { title && <div className="page__info">
-                    <h1 className="p-name">{title}</h1>
-                    {date && <h3 className="page__info__date">{date}</h3>}
-                </div> }
-                <div className="page__body e-content">{props.children}</div>
-            </article>
-        </Layout>
-    );
+	return (
+		<Layout siteTitle={siteTitle} pageTitle={title}>
+			<article className="page h-entry">
+				{title && (
+					<div className="page__info">
+						<h1 className="p-name">{title}</h1>
+						{date && <h3 className="page__info__date">{date}</h3>}
+					</div>
+				)}
+				<div className="page__body e-content">{props.children}</div>
+			</article>
+		</Layout>
+	);
 }
---
+++ pages/404.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import Page from "../components/Page"
+
+const SSR = typeof window === "undefined";
+
+export default function FourOhFour() {
+	const setupEditRoute = () => {
+		if (window.location.href.endsWith("/edit")) {
+			const baseUrl =
+				"https://github.com/garritfra/garrit.xyz/edit/main/content";
+			const filePath = window.location.pathname.replace(/\/edit$/, "");
+			const url = `${baseUrl}${filePath}.md`;
+			window.location.href = url;
+		}
+	};
+
+	!SSR && setupEditRoute();
+	return (
+		<Page>
+			<h1>404 - Page Not Found</h1>
+			<Link href="/">
+				<a>Go back home</a>
+			</Link>
+		</Page>
+	);
+}
