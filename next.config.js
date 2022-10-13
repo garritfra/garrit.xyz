7d4b25b5887a5d7a665d39218ee456b41d85dc33
Garrit Franke <garrit@slashdev.space>
2022-10-13 09:09:39 +0200

    Replace react with preact

--- next.config.js
+++ next.config.js
@@ -10,11 +10,19 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-  webpack: function (config) {
+  webpack: function (config, { dev, isServer }) {
     config.module.rules.push({
       test: /\.md$/,
       use: "raw-loader",
     });
+
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      })
+    }
     return config;
   },
 };
