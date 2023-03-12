dfaa04834b853cd04574e8c27c0c033a6c606ab0
Garrit Franke <garrit@slashdev.space>
2023-03-12 13:51:15 +0100

    Use posts service in rss script

---
+++ lib/rss.ts
@@ -0,0 +1,68 @@
+import { getPosts } from "./posts";
+
+import fs from "fs/promises";
+import path from "path";
+
+import matter from "gray-matter";
+import xmlFormat from 'xml-formatter';
+import rfc822Date from "rfc822-date";
+import { markdown } from "markdown";
+
+const buildRss = async () => {
+	const posts = await getPosts();
+
+	const getRssXml = (blogPosts) => {
+		const { rssItemsXml, latestPostDate } = blogPostsRssXml(blogPosts);
+		return `<?xml version="1.0" ?>
+<rss version="2.0">
+	<channel>
+		<title>garrit.xyz</title>
+		<link>https://garrit.xyz</link>
+		<description>Garrit Franke</description>
+		<language>en</language>
+		<lastBuildDate>${rfc822Date(
+						new Date(latestPostDate)
+					)}</lastBuildDate>
+		${rssItemsXml}
+	</channel>
+</rss>`;
+	};
+
+	const blogPostsRssXml = (blogPosts) => {
+		let latestPostDate = "";
+		let rssItemsXml = "";
+		blogPosts
+			.filter((post) => !post.slug.startsWith("_"))
+			// Ternary operator is used to fix chromium sorting
+			// See: https://stackoverflow.com/a/36507611
+			.sort((a, b) => (a.frontmatter.date < b.frontmatter.date ? 1 : -1))
+			.forEach((post) => {
+				const postDate = Date.parse(post.frontmatter.date);
+				if (!latestPostDate || postDate > Date.parse(latestPostDate)) {
+					latestPostDate = post.frontmatter.date;
+				}
+				rssItemsXml += `
+<item>
+	<title>${post.frontmatter.title}</title>
+	<link>
+		https://garrit.xyz/posts/${post.slug}?utm_source=rss
+	</link>
+
+	<pubDate>${rfc822Date(new Date(postDate))}</pubDate>
+	<description>
+	<![CDATA[${markdown.toHTML(post.markdownBody)}]]>
+	</description>
+</item>`;
+			});
+		return {
+			rssItemsXml,
+			latestPostDate,
+		};
+	};
+
+	const feedPath = path.join(__dirname, "../public/rss.xml");
+
+	await fs.writeFile(feedPath, xmlFormat(getRssXml(posts)), { flag: "w" });
+};
+
+buildRss();
